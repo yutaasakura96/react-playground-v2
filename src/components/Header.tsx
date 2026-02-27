c4c28c967545c9084e2f774f2e36c462dda01813
yuta asakura <yuta.asakura.se@gmail.com>
2026-02-27 14:32:55 +0900

    fix: update image source in Header component to use imported asset

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import reactImg from '../assets/react-core-concepts.png';
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max: number) {
@@ -8,7 +10,7 @@ const Header = () => {
   const description = reactDescriptions[genRandomInt(reactDescriptions.length)];
   return (
     <header>
-      <img src='src/assets/react-core-concepts.png' alt='Stylized Atom' />
+      <img src={reactImg} alt='Stylized Atom' />
       <h1>React Essentials</h1>
       <p>{description} React concepts you will need for almost any app you are going to build!</p>
     </header>
