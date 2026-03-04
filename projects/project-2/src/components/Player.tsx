a16ec9d2c315bbcf0d126ff469d0f990d94ca87e
yuta asakura <yuta.asakura.se@gmail.com>
2026-03-04 21:48:07 +0900

    feat: implement editable player name functionality in Player component

--- projects/project-2/src/components/Player.tsx
+++ projects/project-2/src/components/Player.tsx
@@ -1,15 +1,27 @@
+import { useState } from 'react';
+
 interface PlayerProps {
   name: string;
   symbol: string;
 }
 const Player = ({ name, symbol }: PlayerProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  let playerName = <span className='player-name'>{name}</span>;
+
+  if (isEditing) {
+    playerName = <input className='player-name' type='text' required />;
+  }
   return (
     <li>
       <span className='player'>
-        <span className='player-name'>{name}</span>
+        {playerName}
         <span className='player-symbol'>{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
   );
 };
