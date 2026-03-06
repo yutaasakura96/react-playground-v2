cca4e1948d3685430c035bdf5cbe3992de6efa24
yuta asakura <yuta.asakura.se@gmail.com>
2026-03-06 10:42:25 +0900

    refactor: update Player component to use initialName prop and manage player name state

--- projects/project-2/src/components/Player.tsx
+++ projects/project-2/src/components/Player.tsx
@@ -1,24 +1,36 @@
 import { useState } from 'react';
 
 interface PlayerProps {
-  name: string;
+  initialName: string;
   symbol: string;
 }
-const Player = ({ name, symbol }: PlayerProps) => {
+const Player = ({ initialName, symbol }: PlayerProps) => {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
   const handleEditClick = () => {
     setIsEditing((editing) => !editing);
   };
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPlayerName(event.target.value);
+  };
 
-  let playerName = <span className='player-name'>{name}</span>;
+  let editablePlayerName = <span className='player-name'>{playerName}</span>;
 
   if (isEditing) {
-    playerName = <input className='player-name' type='text' required value={name} />;
+    editablePlayerName = (
+      <input
+        className='player-name'
+        type='text'
+        required
+        value={playerName}
+        onChange={handleChange}
+      />
+    );
   }
   return (
     <li>
       <span className='player'>
-        {playerName}
+        {editablePlayerName}
         <span className='player-symbol'>{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
