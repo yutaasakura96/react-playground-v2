9e91f09ecdda1049909e9d8dfaeb4c3d571b88e1
yuta asakura <yuta.asakura.se@gmail.com>
2026-03-06 12:53:09 +0900

    feat: implement game state management in GameBoard component with square selection functionality

--- projects/project-2/src/components/GameBoard.tsx
+++ projects/project-2/src/components/GameBoard.tsx
@@ -1,16 +1,32 @@
-const initialGameBoard = [
+import { useState } from 'react';
+
+type PlayerSymbol = 'X' | 'O';
+type GameBoardType = (PlayerSymbol | null)[][];
+
+const initialGameBoard: GameBoardType = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
+
 const GameBoard = () => {
+  const [gameBoard, setGameBoard] = useState<GameBoardType>(initialGameBoard);
+
+  const handleSelectSquare = (rowIndex: number, colIndex: number) => {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = prevGameBoard.map((innerArray) => [...innerArray]);
+      updatedBoard[rowIndex][colIndex] = 'X';
+      return updatedBoard;
+    });
+  };
+
   return (
     <ol id='game-board'>
-      {initialGameBoard.map((row, rowIndex) => (
-        <ol>
+      {gameBoard.map((row, rowIndex) => (
+        <ol key={rowIndex}>
           {row.map((playerSymbol, colIndex) => (
             <li key={`${rowIndex}-${colIndex}`}>
-              <button>{playerSymbol}</button>
+              <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
             </li>
           ))}
         </ol>
